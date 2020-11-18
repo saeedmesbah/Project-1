491b17bce7b37a30145d8b23a31a8897511d915d
saeedmesbah <58102785+saeedmesbah@users.noreply.github.com>
2020-11-18 19:00:31 +0330

    Update Organizer.java

--- Answer/Organizer.java
+++ Answer/Organizer.java
@@ -7,40 +7,40 @@ public class Organizer {
     Player play1=new Player("Axis");
     
     
-    Soldier s1=new Soldier();
-    Soldier s2=new Soldier();
-    Soldier s3=new Soldier();
-    Soldier s4=new Soldier();
-    Soldier s5=new Soldier();
-    Soldier s6=new Soldier();
-    Soldier s7=new Soldier();
+    Soldier s1=new Soldier(4);
+    Soldier s2=new Soldier(4);
+    Soldier s3=new Soldier(4);
+    Soldier s4=new Soldier(4);
+    Soldier s5=new Soldier(4);
+    Soldier s6=new Soldier(4);
+    Soldier s7=new Soldier(4);
     
-    Tank t1=new Tank();
-    Tank t2=new Tank();
-    Tank t3=new Tank();
-    Tank t4=new Tank();
-    Tank t5=new Tank();
-    Tank t6=new Tank();
+    Tank t1=new Tank(4);
+    Tank t2=new Tank(4);
+    Tank t3=new Tank(4);
+    Tank t4=new Tank(4);
+    Tank t5=new Tank(4);
+    Tank t6=new Tank(4);
     
     
     
     Player play2=new Player("Allied");
     
-    Soldier s8=new Soldier();
-    Soldier s9=new Soldier();
-    Soldier s10=new Soldier();
-    Soldier s11=new Soldier();
-    Soldier s12=new Soldier();
-    Soldier s13=new Soldier();
-    Soldier s14=new Soldier();
-    Soldier s15=new Soldier();
+    Soldier s8=new Soldier(4);
+    Soldier s9=new Soldier(4);
+    Soldier s10=new Soldier(4);
+    Soldier s11=new Soldier(4);
+    Soldier s12=new Soldier(4);
+    Soldier s13=new Soldier(4);
+    Soldier s14=new Soldier(4);
+    Soldier s15=new Soldier(4);
     
-    Tank t7=new Tank();
-    Tank t8=new Tank();
-    Tank t9=new Tank();
+    Tank t7=new Tank(3);
+    Tank t8=new Tank(3);
+    Tank t9=new Tank(3);
     
-    Artillery a1=new Artillery();
-    Artillery a2=new Artillery();
+    Artillery a1=new Artillery(2);
+    Artillery a2=new Artillery(2);
     
     public Organizer(){
         
