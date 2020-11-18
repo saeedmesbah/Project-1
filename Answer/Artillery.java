b3a60ebefd5338057a96f5fb79dc855bd0ccf125
saeedmesbah <58102785+saeedmesbah@users.noreply.github.com>
2020-11-18 18:58:38 +0330

    Update Artillery.java

--- Answer/Artillery.java
+++ Answer/Artillery.java
@@ -1,4 +1,18 @@
 
 public class Artillery extends Forces{
     
+    public Artillery(int health){
+        super(health);
+    }
+    
+    @Override
+    public void doAction1(){
+        
+    }
+    
+    @Override
+    public void decreaseHealth(){
+        super.decreaseHealth();
+        super.setName(super.getName()+"A");
+    }
 }
