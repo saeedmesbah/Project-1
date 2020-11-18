737b0658f4e3852a4c9b3bfcbc05fb9168aaeddb
saeedmesbah <58102785+saeedmesbah@users.noreply.github.com>
2020-11-18 19:02:06 +0330

    Update Tank.java

--- Answer/Tank.java
+++ Answer/Tank.java
@@ -1,4 +1,21 @@
 
 public class Tank extends Forces{
     
+    
+    public Tank(int health){
+        super(health);
+        super.setName(super.getName()+"T");
+    }
+    
+    @Override
+    public void doAction1(){
+        
+    }
+    
+    @Override
+    public void decreaseHealth(){
+        super.decreaseHealth();
+        
+    }
+    
 }
