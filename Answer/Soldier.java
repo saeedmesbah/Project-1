15d38bfa0ff27eff4fbe57ccaaee52501fd70593
saeedmesbah <58102785+saeedmesbah@users.noreply.github.com>
2020-11-18 19:01:40 +0330

    Update Soldier.java

--- Answer/Soldier.java
+++ Answer/Soldier.java
@@ -1,4 +1,26 @@
 
 public class Soldier extends Forces{
     
+    
+    public Soldier(int health){
+        super(health);
+        super.setName(super.getName()+"S");
+    }
+    
+    
+    
+    @Override
+    public void doAction1(){
+        
+    }
+    
+    public void doAction2(){
+        
+    }
+    
+    @Override
+    public void decreaseHealth(){
+        super.decreaseHealth();
+    }
+    
 }
