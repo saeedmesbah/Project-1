df097637399bcacf508994ee7b9ced907bd29a21
saeedmesbah <58102785+saeedmesbah@users.noreply.github.com>
2020-11-18 18:59:18 +0330

    Update Forces.java

--- Answer/Forces.java
+++ Answer/Forces.java
@@ -5,4 +5,31 @@ public class Forces {
     
     
     private int health;
+    private String name;
+    
+    public Forces(int health){
+        this.health=health;
+        name=health+"";
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name=name;
+    }
+    
+    /**
+     *
+     */
+    public void doAction1(){
+        
+    }
+    
+    public void decreaseHealth(){
+        health--;
+        name=health+"";
+    }
+    
 }
